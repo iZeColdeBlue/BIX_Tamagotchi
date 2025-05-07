c9365f9761c24705bea38ab6f118005f70f1f3a9
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-05-07 16:28:46 +0200

    upload JS files

---
+++ js/buttonClicks.js
@@ -0,0 +1,30 @@
+const loveButton = document.getElementById("loveB");
+const hungerButton = document.getElementById("hungerB");
+const hygieneButton = document.getElementById("hygieneB");
+
+const socket = new WebSocket("ws://localhost:8025/test");
+
+socket.onopen = function() {
+    console.log('Connected to Processing server');
+};
+
+//functions
+function loveClicked(){
+    console.log("You sent LOVE to the alien!");
+    socket.send("LOVE")
+}
+
+function hungerClicked(){
+    console.log("You sent FOOD to the alien!");
+    socket.send("FOOD")
+}
+
+function hygieneClicked(){
+    console.log("You cleaned the alien!");
+    socket.send("SOAP")
+}
+
+//called functions 
+loveButton.addEventListener("click", loveClicked);
+hungerButton.addEventListener("click", hungerClicked);
+hygieneButton.addEventListener("click", hygieneClicked);
