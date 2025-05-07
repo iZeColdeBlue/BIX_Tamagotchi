4cb2d170b0f88b431275ec96a889f11e17e0e84b
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-05-07 16:29:39 +0200

    Update Websocket

--- js/buttonClicks.js
+++ js/buttonClicks.js
@@ -2,7 +2,7 @@ const loveButton = document.getElementById("loveB");
 const hungerButton = document.getElementById("hungerB");
 const hygieneButton = document.getElementById("hygieneB");
 
-const socket = new WebSocket("ws://localhost:8025/test");
+const socket = new WebSocket("ws:https://5a93-2001-871-263-95f1-38b2-d270-e4be-1da6.ngrok-free.app");
 
 socket.onopen = function() {
     console.log('Connected to Processing server');
