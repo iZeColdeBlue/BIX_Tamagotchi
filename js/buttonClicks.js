9e0109ecb2d0cc7d4c9a3ef9c09f20483d15d3a4
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-05-11 13:39:50 +0200

    addd Graph variables
    Basics to adjust the fill of the graphs

--- js/buttonClicks.js
+++ js/buttonClicks.js
@@ -4,6 +4,16 @@ const hygieneButton = document.getElementById("hygieneB");
 
 const socket = new WebSocket("wss://settled-closely-moccasin.ngrok-free.app/test");
 
+const currentLove = 100.00;
+const currentHunger = 100.00;
+const currentHygiene = 100.00;
+
+window.onload = () => {
+    document.documentElement.style.setProperty('--loveGraphFill', currentLove);
+    document.documentElement.style.setProperty('--hungerGraphFill', currentHunger);
+    document.documentElement.style.setProperty('--hygieneGraphFill', currentHygiene);
+}
+
 socket.onopen = function() {
     console.log('Connected to Processing server');
 };
