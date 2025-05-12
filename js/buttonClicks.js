eac695dfd98628565e14237e96e5ba07e82e8461
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-05-12 22:37:36 +0200

    UI updates and blocks users when button is clicked

--- js/buttonClicks.js
+++ js/buttonClicks.js
@@ -2,39 +2,139 @@ const loveButton = document.getElementById("loveB");
 const hungerButton = document.getElementById("hungerB");
 const hygieneButton = document.getElementById("hygieneB");
 
+const blockTimer = document.getElementById('timer');
+const UNLOCK_KEY = "uiUnlockTime";
+const title = document.getElementById('pageTitle');
+
 const socket = new WebSocket("wss://settled-closely-moccasin.ngrok-free.app/test");
 
-const currentLove = 100.00;
-const currentHunger = 100.00;
-const currentHygiene = 100.00;
+let currentLove = 50.00;
+let currentHunger = 25.00;
+let currentHygiene = 40.00;
 
-window.onload = () => {
-    document.documentElement.style.setProperty('--loveGraphFill', currentLove);
-    document.documentElement.style.setProperty('--hungerGraphFill', currentHunger);
-    document.documentElement.style.setProperty('--hygieneGraphFill', currentHygiene);
-}
+let lockDurration;
+let unlockTimestamp;
 
+window.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.style.setProperty('--loveGraphFill', currentLove + '%');
+    document.documentElement.style.setProperty('--hungerGraphFill', currentHunger + '%');
+    document.documentElement.style.setProperty('--hygieneGraphFill', currentHygiene + '%');
+
+    const storedUnlockTimestamp = localStorage.getItem(UNLOCK_KEY);
+    if(!storedUnlockTimestamp){
+        unlockUI();
+    }
+    const remainingTime = storedUnlockTimestamp - Date.now();
+    if (remainingTime > 0) {
+        lockDurration = remainingTime;
+        blockUI();
+        updateTimer();
+    } else {
+        localStorage.removeItem(UNLOCK_KEY);
+        unlockUI();
+    }
+
+});
+
+//socket connection
 socket.onopen = function() {
     console.log('Connected to Processing server');
 };
 
+
 //functions
 function loveClicked(){
-    console.log("You sent LOVE to the alien!");
-    socket.send("LOVE")
+    if(loveButton.classList.contains('button')){
+        if(currentLove < 100){
+            console.log("You sent LOVE to the alien!");
+            socket.send("LOVE");
+            currentLove += 5;
+            document.documentElement.style.setProperty('--loveGraphFill', currentLove + '%');
+        }
+    } 
 }
 
 function hungerClicked(){
-    console.log("You sent FOOD to the alien!");
-    socket.send("FOOD")
+    if(hungerButton.classList.contains('button')){
+        if(currentHunger < 100){
+            console.log("You sent FOOD to the alien!");
+            socket.send("FOOD");
+            currentHunger += 5;
+            document.documentElement.style.setProperty('--hungerGraphFill', currentHunger + '%');
+        }
+    }
 }
 
 function hygieneClicked(){
-    console.log("You cleaned the alien!");
-    socket.send("SOAP")
+    if(hygieneButton.classList.contains('button')){
+        if(currentHygiene < 100){
+            console.log("You cleaned the alien!");
+            socket.send("SOAP");
+            currentHygiene += 5;
+            document.documentElement.style.setProperty('--hygieneGraphFill', currentHygiene + '%');
+        }
+    }
 }
 
-//called functions 
-loveButton.addEventListener("click", loveClicked);
-hungerButton.addEventListener("click", hungerClicked);
-hygieneButton.addEventListener("click", hygieneClicked);
+function blockUI(){
+    hygieneButton.classList.remove('button');
+    hungerButton.classList.remove('button');
+    loveButton.classList.remove('button');
+
+    hygieneButton.classList.add('blocked');
+    hungerButton.classList.add('blocked');
+    loveButton.classList.add('blocked');
+
+    blockTimer.classList.remove('hidden');
+    title.textContent = "Wait to send more love!";
+}
+
+function unlockUI(){
+    hygieneButton.classList.remove('blocked');
+    hungerButton.classList.remove('blocked');
+    loveButton.classList.remove('blocked');
+
+    hygieneButton.classList.add('button');
+    hungerButton.classList.add('button');
+    loveButton.classList.add('button');
+    title.textContent = "Take care of our friendly alien!";
+}
+
+function updateTimer(){
+    if(!lockDurration){
+        lockDurration = 5 * 60 * 1000; 
+    }
+    unlockTimestamp = Date.now() + lockDurration;
+    localStorage.setItem(UNLOCK_KEY, unlockTimestamp);
+    const timerInterval = setInterval(() => {
+        const currentTime = Date.now();
+        const timeLeft = unlockTimestamp - currentTime;
+
+        if(timeLeft <= 0){
+            clearInterval(timerInterval);
+            blockTimer.classList.add('hidden');
+            unlockUI();
+        } else {
+            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+            blockTimer.textContent = `0${minutes}:${seconds}`;
+        }
+    }, 1000);
+}
+
+//called functions
+loveButton.addEventListener("click", () => {
+    loveClicked();
+    blockUI();
+    updateTimer();
+});
+hungerButton.addEventListener("click", () => {
+    hungerClicked();
+    blockUI();
+    updateTimer();
+});
+hygieneButton.addEventListener("click", () => {
+    hygieneClicked();
+    blockUI();
+    updateTimer();
+});
