442be9b00c9e43b61376016e3df62db5d0b06876
iZeColdeBlue <schnurrerjakob@gmail.com>
2025-05-13 08:08:50 +0200

    small visual changes (responsive Design) + alert added

--- js/buttonClicks.js
+++ js/buttonClicks.js
@@ -6,6 +6,9 @@ const blockTimer = document.getElementById('timer');
 const UNLOCK_KEY = "uiUnlockTime";
 const title = document.getElementById('pageTitle');
 
+const alert = document.getElementById('warningAlert');
+const alertText = document.getElementById('alertText');
+
 const socket = new WebSocket("wss://settled-closely-moccasin.ngrok-free.app/test");
 //start websocket connection: ngrok http --url=settled-closely-moccasin.ngrok-free.app 8025
 
@@ -16,6 +19,7 @@ let currentHygiene;
 let lockDurration;
 let unlockTimestamp;
 
+// update the graph with the current values - fetch data from the server
 async function updateGraph() {
     socket.send("GET");
 
@@ -34,8 +38,23 @@ async function updateGraph() {
     document.documentElement.style.setProperty('--loveGraphFill', currentLove + '%');
     document.documentElement.style.setProperty('--hungerGraphFill', currentHunger + '%');
     document.documentElement.style.setProperty('--hygieneGraphFill', currentHygiene + '%');
+
+    if(currentLove < 10 || currentHunger < 10 || currentHygiene < 10){
+        alert.classList.remove('hidden');
+        alertText.textContent = "The Alien needs:";
+        if(currentLove < 10){
+            alertText.textContent += " LOVE";
+        }
+        if(currentHunger < 10){;
+            alertText.textContent += " FOOD";
+        }
+        if(currentHygiene < 10){
+            alertText.textContent += " SOAP";
+        }
+    }   
 }
 
+//runs when the pages content is loaded
 window.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty('--loveGraphFill', currentLove + '%');
     document.documentElement.style.setProperty('--hungerGraphFill', currentHunger + '%');
@@ -57,6 +76,7 @@ window.addEventListener('DOMContentLoaded', () => {
     
 });
 
+// runs when the page is loaded
 window.addEventListener('load', () => {
     setInterval(updateGraph, 1000); // Update 10 second
 });
@@ -64,10 +84,16 @@ window.addEventListener('load', () => {
 //socket connection
 socket.onopen = function() {
     console.log('Connected to Processing server');
+    alert.classList.add('hidden');
 };
 
+//if the connection is closed
+socket.onerror = function() {
+    alert.classList.remove('hidden');
+    alertText.textContent = "Could not connect to the server.";
+}
 
-//functions
+//send data to server - called when buttons are clicked
 function loveClicked(){
     if(loveButton.classList.contains('button')){
         if(currentLove < 100){
@@ -101,6 +127,7 @@ function hygieneClicked(){
     }
 }
 
+// Function to block the UI and show the timer - called when buttons are clicked
 function blockUI(){
     hygieneButton.classList.remove('button');
     hungerButton.classList.remove('button');
@@ -114,6 +141,7 @@ function blockUI(){
     title.textContent = "Wait to send more love!";
 }
 
+// Function to unlock the UI after the timer ends
 function unlockUI(){
     hygieneButton.classList.remove('blocked');
     hungerButton.classList.remove('blocked');
@@ -125,6 +153,7 @@ function unlockUI(){
     title.textContent = "Take care of our friendly alien!";
 }
 
+// Function to update the timer - called when buttons are clicked
 function updateTimer(){
     if(!lockDurration){
         lockDurration = 5 * 60 * 1000;  //<- change to block users for shorter time (5 min for real; 1 min for testing)
@@ -147,19 +176,21 @@ function updateTimer(){
     }, 1000);
 }
 
-//called functions
+//called functions - if buttons are clicked
 loveButton.addEventListener("click", () => {
     loveClicked();
     blockUI();
     updateTimer();
     updateGraph();
 });
+
 hungerButton.addEventListener("click", () => {
     hungerClicked();
     blockUI();
     updateTimer();
     updateGraph();
 });
+
 hygieneButton.addEventListener("click", () => {
     hygieneClicked();
     blockUI();
